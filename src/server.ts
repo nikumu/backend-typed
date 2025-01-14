af0cace59332d8bfff352cfff6c4e03d817b4653
nikumu <andrecarlosbb@gmail.com>
2025-01-14 10:06:04 -0300

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,16 @@
 import { fastify } from 'fastify'
-import 
+import { fastifyCors } from '@fastify/cors'
+import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
+
+const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, { origin '*' })
+
+
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP server running!')
+})
