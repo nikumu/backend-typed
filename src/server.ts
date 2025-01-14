13fe56aa544f44917d368686df95ad89820d8437
nikumu <andrecarlosbb@gmail.com>
2025-01-14 10:21:19 -0300

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
 const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(fastifyCors, { origin '*' })
+app.register(fastifyCors, { origin: '*' })
 
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'Typed API',
+            version: '1.0.0',
+        }
+    }
+})
 
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+})
+
+app.get('/', () => {
+    return 'Hello, World!'
+})
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
